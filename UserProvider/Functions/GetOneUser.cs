bbcb55ad2bd4303ade3d5a98c698bf3b25f2daa5
Anas-Karoum <anas-karoum@outlook.com>
2024-06-08 11:33:34 +0200

    upphiften är klar

---
+++ UserProvider/Functions/GetOneUser.cs
@@ -0,0 +1,34 @@
+using Data.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace UserProvider.Functions
+{
+    public class GetOneUser(ILogger<GetOneUser> logger, DataContext context)
+    {
+        private readonly ILogger<GetOneUser> _logger = logger;
+        private readonly DataContext _context = context;
+
+        [Function("GetOneUser")]
+        public async Task <IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "edituser{id}")] HttpRequest req, string id)
+        {
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+                if (user != null)
+                {
+                    return new OkObjectResult(user);
+                }
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"GetOneSubscriber :: {ex.Message}");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
